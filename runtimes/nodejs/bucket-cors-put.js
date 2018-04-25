cdd0a7677bcff81103bc89cd1595de9d91c84b85
Belinda Vennam <beemarie88@gmail.com>
2018-04-25 14:13:13 -0500

    CORS CRUD Operations (#8)
    * cors get
    
    * bucket cors delete and put
    
    * update manifest and tests
    
    * updating bucket cors put
    
    * adding python cors
    
    * update comments cors actions

---
+++ runtimes/nodejs/bucket-cors-put.js
@@ -0,0 +1,69 @@
+/**
+ * This action will put CORS Configuration information for a Cloud Object Storage bucket.
+ * If the Cloud Object Storage service is not bound to this action or to the package
+ * containing this action, then you must provide the service information as argument
+ * input to this function.
+ * Cloud Functions actions accept a single parameter, which must be a JSON object.
+ *
+ * In this case, the args variable will look like:
+ *   {
+ *     "bucket": "your COS bucket name",
+ *     "corsConfig":{
+ *       "CORSRules":[{
+ *         "AllowedHeaders":["*"],
+ *         "AllowedMethods":["PUT", "GET", "DELETE"],
+ *         "AllowedOrigins":["*"]
+ *      }]}
+ *   }
+ */
+const CloudObjectStorage = require('ibm-cos-sdk');
+
+async function main(args) {
+  const { cos, params } = getParamsCOS(args, CloudObjectStorage);
+
+  let response;
+  const result = {
+    bucket: params.bucket,
+  };
+  try {
+    response = await cos.putBucketCors({
+      Bucket: params.bucket,
+      CORSConfiguration: params.corsConfig,
+    }).promise();
+  } catch (err) {
+    console.log(err);
+    result.message = err.message;
+    throw result;
+  }
+  result.body = response;
+  return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getParamsCOS(args, COS) {
+  const { bucket, corsConfig } = args;
+  const endpoint = args.endpoint || 's3-api.us-geo.objectstorage.softlayer.net';
+  const ibmAuthEndpoint = args.ibmAuthEndpoint || 'https://iam.ng.bluemix.net/oidc/token';
+  const apiKeyId = args.apikey || args.apiKeyId || args.__bx_creds['cloud-object-storage'].apikey;
+  const serviceInstanceId = args.resource_instance_id || args.serviceInstanceId || args.__bx_creds['cloud-object-storage'].resource_instance_id;
+
+  const params = {};
+  params.bucket = bucket;
+  params.corsConfig = corsConfig;
+  const cos = new COS.S3({
+    endpoint, ibmAuthEndpoint, apiKeyId, serviceInstanceId,
+  });
+  return { cos, params };
+}
---
+++ runtimes/python/bucket-cors-get.py
@@ -0,0 +1,48 @@
+# This action will get CORS Configuration for a Cloud Object Storage bucket.
+# If the Cloud Object Storage service is not bound to this action or to the package
+# containing this action, then you must provide the service information as argument
+# input to this function.
+# Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# In this case, the args variable will look like:
+#   {
+#     "bucket": "your COS bucket name",
+#   }
+
+import sys
+import json
+import ibm_boto3
+from ibm_botocore.client import Config
+
+def main(args):
+  resultsGetParams = getParamsCOS(args)
+  cos = resultsGetParams['cos']
+  bucket = resultsGetParams['params']['bucket']
+  try:
+    object = cos.get_bucket_cors(
+    Bucket=bucket,
+    )
+  except ClientError as e:
+    print(e)
+    raise e
+
+  return {
+    'bucket':bucket,
+    'data': str(object)
+    }
+
+
+def getParamsCOS(args):
+  endpoint = args.get('endpoint','https://s3-api.us-geo.objectstorage.softlayer.net')
+  api_key_id = args.get('apikey', args.get('apiKeyId', args.get('__bx_creds', {}).get('cloud-object-storage', {}).get('apikey', '')))
+  service_instance_id = args.get('resource_instance_id', args.get('serviceInstanceId', args.get('__bx_creds', {}).get('cloud-object-storage', {}).get('resource_instance_id', '')))
+  ibm_auth_endpoint = args.get('ibmAuthEndpoint', 'https://iam.ng.bluemix.net/oidc/token')
+  cos = ibm_boto3.client('s3',
+    ibm_api_key_id=api_key_id,
+    ibm_service_instance_id=service_instance_id,
+    ibm_auth_endpoint=ibm_auth_endpoint,
+    config=Config(signature_version='oauth'),
+    endpoint_url=endpoint)
+  params = {}
+  params['bucket'] = args['bucket']
+  return {'cos':cos, 'params':params}
