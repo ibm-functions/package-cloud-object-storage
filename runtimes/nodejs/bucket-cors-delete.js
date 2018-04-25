efa09163062ac302961b01b1f0b7ce4535848d95
Belinda Vennam <beemarie@us.ibm.com>
2018-04-24 23:25:16 -0500

    bucket cors delete and put

---
+++ runtimes/nodejs/bucket-cors-delete.js
@@ -0,0 +1,63 @@
+/**
+ * This action will write to Cloud Object Storage.  If the Cloud Object Storage
+ * service is not bound to this action or to the package containing this action,
+ * then you must provide the service information as argument input to this function.
+ * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+ *
+ * In this case, the args variable will look like:
+ *   {
+ *     "bucket": "your COS bucket name",
+ *     "key": "Name of the object to write",
+ *     "body": "Body of the object to write"
+ *   }
+ */
+const CloudObjectStorage = require('ibm-cos-sdk');
+
+async function main(args) {
+  const { cos, params } = getParamsCOS(args, CloudObjectStorage);
+
+  let response;
+  const result = {
+    bucket: params.bucket,
+  };
+  try {
+    response = await cos.deleteBucketCors({ Bucket: params.bucket }).promise();
+  } catch (err) {
+    console.log(err)
+    result.message = err.message;
+    throw result;
+  }
+  result.body = response;
+  return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getParamsCOS(args, COS) {
+
+  const bucket = args.bucket || args.Bucket;
+  const endpoint = args.endpoint || 's3-api.us-geo.objectstorage.softlayer.net';
+  const ibmAuthEndpoint = args.ibmAuthEndpoint || 'https://iam.ng.bluemix.net/oidc/token';
+  const apiKeyId = args.apikey || args.apiKeyId || args.__bx_creds['cloud-object-storage'].apikey;
+  const serviceInstanceId = args.resource_instance_id || args.serviceInstanceId || args.__bx_creds['cloud-object-storage'].resource_instance_id;
+
+  const params = {};
+  params.bucket = bucket;
+  const cos = new COS.S3({
+    endpoint, ibmAuthEndpoint, apiKeyId, serviceInstanceId,
+  });
+  return { cos, params };
+}
