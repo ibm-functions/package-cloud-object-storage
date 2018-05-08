0b44cc60ce961c947618968bd4039c1609a232dd
Carlos Santana <csantanapr@apache.org>
2018-05-08 12:13:43 -0400

    update travis scripts (#16)
    * update travis scripts
    
    * fix python kind for travis
    
    * temp skip scanscode
    
    * moar updates

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -1,61 +1,36 @@
 #!/bin/bash
 # Build script for Travis-CI.
 
-SCRIPTDIR=$(cd $(dirname "$0") && pwd)
-ROOTDIR="$SCRIPTDIR/../../.."
-WHISKDIR="$ROOTDIR/openwhisk"
-PACKAGESDIR="$WHISKDIR/catalog/extra-packages"
-IMAGE_PREFIX="testing"
+set -ex
 
-# Set Environment
+# Build script for Travis-CI.
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+ROOTDIR="$SCRIPTDIR/../.."
+WHISKDIR="$ROOTDIR/../openwhisk"
+UTILDIR="$ROOTDIR/../incubator-openwhisk-utilities"
+
 export OPENWHISK_HOME=$WHISKDIR
 
-cd $WHISKDIR
+IMAGE_PREFIX="testing"
 
-tools/build/scanCode.py "$SCRIPTDIR/../.."
+# run scancode using the ASF Release configuration
+# FIXME: Setup ibm-functions scancode cfg
+#cd $UTILDIR
+#scancode/scanCode.py --config scancode/ASF-Release-v2.cfg $ROOTDIR
 
-# Build Openwhisk
-./gradlew distDocker -PdockerImagePrefix=${IMAGE_PREFIX}
-
+#pull down images
+docker pull openwhisk/controller
+docker tag openwhisk/controller ${IMAGE_PREFIX}/controller
+docker pull openwhisk/invoker
+docker tag openwhisk/invoker ${IMAGE_PREFIX}/invoker
+docker pull openwhisk/nodejs6action
+docker tag openwhisk/nodejs6action ${IMAGE_PREFIX}/nodejs6action
 docker pull ibmfunctions/action-nodejs-v8
 docker tag ibmfunctions/action-nodejs-v8 ${IMAGE_PREFIX}/action-nodejs-v8
-
 docker pull ibmfunctions/action-python-v3
 docker tag ibmfunctions/action-python-v3 ${IMAGE_PREFIX}/action-python-v3
 
-cd $WHISKDIR/ansible
-
-# Deploy Openwhisk
-ANSIBLE_CMD="ansible-playbook -i environments/local -e docker_image_prefix=${IMAGE_PREFIX}"
-
-$ANSIBLE_CMD setup.yml
-$ANSIBLE_CMD prereq.yml
-$ANSIBLE_CMD couchdb.yml
-$ANSIBLE_CMD initdb.yml
-$ANSIBLE_CMD wipe.yml
-$ANSIBLE_CMD openwhisk.yml
-
-cd $WHISKDIR
-
-#update whisk.properties to add tests/credentials.json file to vcap.services.file, which is needed in tests
-WHISKPROPS_FILE="$WHISKDIR/whisk.properties"
-cat whisk.properties
-
-WSK_CLI=$WHISKDIR/bin/wsk
-AUTH_KEY=$(cat $WHISKDIR/ansible/files/auth.whisk.system)
-EDGE_HOST=$(grep '^edge.host=' $WHISKPROPS_FILE | cut -d'=' -f2)
-
-# Set Environment
-export OPENWHISK_HOME=$WHISKDIR
-
-# Place this template in correct location to be included in packageDeploy
-mkdir -p $PACKAGESDIR/preInstalled/ibm-functions
-cp -r $ROOTDIR/package-cloud-object-storage $PACKAGESDIR/preInstalled/ibm-functions/
-
-# Install the deploy package
-cd $PACKAGESDIR/packageDeploy/packages
-source $PACKAGESDIR/packageDeploy/packages/installCatalog.sh $AUTH_KEY $EDGE_HOST $WSK_CLI
-
-# Test
-cd $ROOTDIR/package-cloud-object-storage
-./gradlew :tests:test
+# Build OpenWhisk
+cd $OPENWHISK_HOME
+TERM=dumb ./gradlew install
---
+++ tools/travis/deploy_cos.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -ex
+
+# Build script for Travis-CI.
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+ROOTDIR="$SCRIPTDIR/../.."
+HOMEDIR="$ROOTDIR/.."
+WHISKDIR="$HOMEDIR/openwhisk"
+# Set Environment
+export OPENWHISK_HOME=${OPENWHISK_HOME:=$WHISKDIR}
+WSK_CLI=${OPENWHISK_HOME}/bin/wsk
+WSK_SYSTEM_AUTH_KEY=$(cat ${OPENWHISK_HOME}/ansible/files/auth.whisk.system)
+WHISK_APIHOST="172.17.0.1"
+
+
+PREINSTALL_DIR=${HOMEDIR}/preInstalled
+
+cd ${ROOTDIR}
+# Place this template in correct location to be included in packageDeploy
+mkdir -p ${PREINSTALL_DIR}/ibm-functions/package-cloud-object-storage
+cp -a runtimes ${PREINSTALL_DIR}/ibm-functions/package-cloud-object-storage/
+
+# Install the deploy package
+cd $HOMEDIR/incubator-openwhisk-package-deploy/packages
+./installCatalog.sh $WSK_SYSTEM_AUTH_KEY $WHISK_APIHOST $WSK_CLI
