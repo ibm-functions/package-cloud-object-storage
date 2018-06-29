1f3d41dfd624f08dbe7db3706ade95811b6218bc
Eric Weiterman <eweiter@us.ibm.com>
2018-06-29 12:04:24 -0400

    fix issues with build (#27)

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -24,12 +24,6 @@ docker pull openwhisk/controller
 docker tag openwhisk/controller ${IMAGE_PREFIX}/controller
 docker pull openwhisk/invoker
 docker tag openwhisk/invoker ${IMAGE_PREFIX}/invoker
-docker pull openwhisk/nodejs6action
-docker tag openwhisk/nodejs6action ${IMAGE_PREFIX}/nodejs6action
-docker pull ibmfunctions/action-nodejs-v8
-docker tag ibmfunctions/action-nodejs-v8 ${IMAGE_PREFIX}/action-nodejs-v8
-docker pull ibmfunctions/action-python-v3
-docker tag ibmfunctions/action-python-v3 ${IMAGE_PREFIX}/action-python-v3
 
 # Build OpenWhisk
 cd $OPENWHISK_HOME
